17430e0d3a78f59751183a00664bac2ca62c3ce0
Kuberow <kuberow.kuberow@gmail.com>
2025-07-13 18:41:21 +0300

    Update startup.lua
    changed to light os 1.0

--- startup.lua
+++ startup.lua
@@ -1,4 +1,3 @@
-term.clear()
 local x, y = term.getSize()
 local midx, midy = x / 2, y / 2
 term.setCursorPos(midx - 3, midy)
@@ -13,11 +12,40 @@ end
 middle("PowerAnything", midy + 1)
 sleep(2)
 term.clear()
-for n = 0, x do
-term.setCursorPos(n, y)
-term.setBackgroundColor(colors.gray)
-term.write(" ")
+function kernel()
+term.setCursorPos(1,1)
+print("LightOS 1.0")
+if fs.exists("root") then
+if fs.exists("root/cmd") then
+else
+fs.makeDir("/root/cmd")
+shell.run("copy /rom/programs/edit.lua /root/cmd/edit.lua")
 end
+else
+print("Installing...")
+print("Recovering Root...")
+sleep(3)
+fs.makeDir("root")
+fs.makeDir("root/cmd")
+print("Rebooting...")
+sleep(2)
+os.reboot()
+end
+shell.run("cd /root/")
+while true do
+cd = shell.dir()
+io.write("["..cd.."] ")
+cmd = io.read()
+cmdnoarg = cmd:match(cmd:match("^(%S+)"))
+if fs.exists("/root/cmd/"..cmdnoarg..".lua") then
+shell.run("/root/cmd/"..cmd)
+else
+shell.run("/root/cmd/"..cmd..".lua")
+print("Unknown command: "..cmdnoarg)
+end
+end
+end
+kernel()
 while true do
 sleep(0)
 local time = os.time()
